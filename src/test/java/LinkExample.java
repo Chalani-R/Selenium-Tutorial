b0b4bb365369e4c4555326f58308eae1fd246b8c
IT22104212KCRSilva <it22104212@my.sliit.lk>
2025-01-22 20:17:37 +0530

    Windoe maximize

--- src/test/java/LinkExample.java
+++ src/test/java/LinkExample.java
@@ -1,13 +1,23 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.sql.Driver;
 
 public class LinkExample {
 
+    @BeforeMethod
+    public void openLinkTestPage(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.leafground.com/link.xhtml;jsessionid=node01ayavh0kwl4ol1lp3u2j7wn1mp8451092.node0https://www.google.com/");
+
+    }
+
     @Test
     public void LinkTests(){
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+
     }
     //(01)Take me to dashboard
 
