bad0ed22026d993b390f84ca749b64fa35865e0b
IT22104212KCRSilva <it22104212@my.sliit.lk>
2025-01-23 11:06:26 +0530

    navigate back

--- src/test/java/LinkExample.java
+++ src/test/java/LinkExample.java
@@ -1,34 +1,29 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.sql.Driver;
+public class LinkExample{
 
-public class LinkExample {
+   WebDriver driver;
 
-    @BeforeMethod
-    public void openLinkTestPage(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.leafground.com/link.xhtml;jsessionid=node01ayavh0kwl4ol1lp3u2j7wn1mp8451092.node0https://www.google.com/");
+@BeforeMethod
+public void openLinkTestPage(){
+   driver = new ChromeDriver();
 
-    }
+   driver.manage().window().maximize();
+   driver.get("https://www.leafground.com/link.xhtml");
 
-    @Test
-    public void LinkTests(){
-
-    }
-    //(01)Take me to dashboard
-
-
-
+}
+   @Test
+   public void LinkTests(){
+      //01)Take me to dashboard
+      WebElement homeLink = driver.findElement(By.linkText("Go to Dashboard"));
+      homeLink.click();
+      driver.navigate().back();
 
 
-
-    //(02)Find my destination
-    //(03)Dublicate Link
-    //(04)Count page Link
-    //(06)Count layout Links
-
+   }
 }
