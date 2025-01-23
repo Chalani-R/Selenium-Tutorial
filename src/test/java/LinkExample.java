93847c4031901f01e7f5cf64e65f88090be96d42
IT22104212KCRSilva <it22104212@my.sliit.lk>
2025-01-23 15:39:48 +0530

    pass the test Working With Links2

--- src/test/java/LinkExample.java
+++ src/test/java/LinkExample.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+
 import java.util.List;
 
 public class LinkExample{
